296168b8bb9e641380234e01b0b6318947a55054
Quentin <quentin@quentb.com>
2019-03-03 19:54:44 -0500

    Lab 5.2 code and explainations added.

--- lab05/lab_2.py
+++ lab05/lab_2.py
@@ -32,8 +32,8 @@ down to the fact that dispite the decent spesificity of the tests, in general th
 is just really low (1%).
 
 Manual Solution:
-P(C|T1^T2) = a(Σ(P(C|t1,t2)
-           = a* <(P(C)P(t1|C)P(t2|C), P(-C)P(t1|-c)P(t2|-c)
+P(C|T1^T2) = a Σ(P(C|t1,t2))
+           = a * <(P(C)P(t1|C)P(t2|C), P(-C)P(t1|-C)P(t2|-C)
            = a <(0.01)(0.9)(0.9), (0.99)(0.2)(0.2)>
            = a <0.0081,0.0396>
            (Normalzing value = 0.0081+0.0396 = 0.0477)
@@ -48,11 +48,22 @@ print("P(C|T1^-T2):")
 print(enumeration_ask('Cancer', dict(Test1=T, Test2=F), cancer).show_approx())
 # Result: False: 0.994, True: 0.00565
 print(elimination_ask('Cancer', dict(Test1=T, Test2=F), cancer).show_approx())
+# Result: False: 0.994, True: 0.00565
 print(gibbs_ask('Cancer', dict(Test1=T, Test2=F), cancer).show_approx())
+# Result: False: 0.994, True: 0.006
 '''
 If ONE test is positive, but the other is negative, its almost certain the patient does NOT have cancer (> 99%)! This is
 because the tests have a low (but non-zero) chance of having a false positive, and the chance of each test's false
 positive (10%) is still _an order of magnitude_ more likely than cancer! A person would be justified in assuming the
 posttive test was a false positive.
+Manual Solution:
+P(C|T1^T2) = a Σ(P(C|t1,-t2))
+           = a * <(P(C)P(t1|C)P(-t2|C), P(-C)P(t1|-C)P(-t2|-C)
+           = a <(0.01)(0.9)(0.1), (0.99)(0.2)(0.8)>
+           = a <0.0009,0.1584>
+           (Normalzing value = 0.0009+0.1584 = 0.1593)
+           = <0.0009/0.1593,0.1584/0.1593> 
+          -------------------------
+           = <0.0056,0.9944>
 '''
 print("===========================================")
