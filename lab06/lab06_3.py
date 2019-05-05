a0a8c3696296383a53ed817286fdd13d10efaa5c
Quentin <quentin@quentb.com>
2019-05-05 16:09:46 -0400

    lab 06 part 3 done

---
+++ lab06/lab06_3.py
@@ -0,0 +1,31 @@
+"""
+
+Lab 6, Exercise 3
+
+Author: Quentin Baker
+Created: April 20, 2019
+Based on examples from https://keras.io/datasets/#boston-housing-price-regression-dataset
+
+"""
+
+from keras.datasets import boston_housing
+
+(x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+
+# a1
+print("Number of test examples:\t\t%s" % len(x_train))
+print("Number of training examples:\t%s\n" % len(x_test))
+
+
+# a2
+def print_data(data_set):
+    print("Rank\t\t%s" % data_set.ndim)
+    print("Shape:\t\t%s" % str(data_set.shape))
+    print("Data Type:\t%s" % data_set.dtype)
+
+
+print('=== Training Data ===')
+print_data(x_train)
+
+print('\n === Testing Data ===')
+print_data(x_test)
